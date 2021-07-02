8d6b9a827ede1d89a98e7759392fdd6d87bcb034
EmiRing <emil.ringwall@gmail.com>
2021-07-02 17:03:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+
+import { TodoContext } from './contexts/Context';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodoCallback = text => {
+    setTodos(todos => {return [{id: uuidv4(), text: text}, ...todos]})
+  }
+
+  const removeTodoCallback = id => {
+    setTodos(todos => todos.filter(todo => todo.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <TodoContext.Provider value={{todos, addTodoCallback, removeTodoCallback}}>
+        <AddTodo />
+        <TodoList />
+      </TodoContext.Provider>
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,19 @@
+import React, {useContext} from 'react'
+import { TodoContext } from '../contexts/Context'
+
+const TodoItem = ({ todo }) => {
+    const { removeTodoCallback} = useContext(TodoContext)
+    
+    const removeTodo = () => {
+        removeTodoCallback(todo.id)
+    }
+
+    return (
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+            {todo.text}
+            <i onClick={removeTodo} className="fas fa-trash"></i>
+        </li>
+    )
+}
+
+export default TodoItem
